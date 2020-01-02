30329d70500c4add95f5b468002c0a96431c23fd
BrianSketch <nico0lutz@gmail.com>
2020-01-02 17:19:27 +0100

    vue components  now work

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,5 +1,5 @@
+@extends('layouts.app')
 
-<passport-clients></passport-clients>
-<passport-authorized-clients>test</passport-authorized-clients>
-<passport-personal-access-tokens>test</passport-personal-access-tokens>
-
+@section('content')
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 Route::view('/test', 'test');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
